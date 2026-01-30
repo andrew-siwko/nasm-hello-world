5568cb56cbf2887214bb10cbefe0bdcac92623f9
Andrew Siwko <asiwko@siwko.org>
2026-01-29 21:12:32 -0500

    added some constants and a define

--- hello-world.asm
+++ hello-world.asm
@@ -1,30 +1,31 @@
-; hello-world.asm - Prints "Hello, World!" in Linux using x86 assembly
+%define call_kernel int 0x80
 
 section .data
-    msg1 db "Hello, World!", 0x0A  ; String with newline
-    len1 equ $ - msg1               ; Length of string
-    msg2 db "How did I get here?", 0x0A  ; String with newline
-    len2 equ $ - msg2               ; Length of string
-
+    msg1        db      "Hello, World!", 0x0A
+    len1        equ     $ - msg1             
+    msg2        db      "How did I get here?", 0x0A
+    len2        equ     $ - msg2               
+    sys_write   equ     4
+    stdout      equ     1
 
 section .text
     global _start
 
 _start:
     ; sys_write (stdout=1, msg1, len1)
-    mov eax, 4        ; syscall number for sys_write
-    mov ebx, 1        ; file descriptor (stdout)
+    mov eax, sys_write
+    mov ebx, stdout
     mov ecx, msg1      ; pointer to message
     mov edx, len1     ; message length
-    int 0x80          ; call kernel
+    call_kernel
 
-    mov eax, 4        ; syscall number for sys_write
-    mov ebx, 1        ; file descriptor (stdout)
-    mov ecx, msg2      ; pointer to message
-    mov edx, len2     ; message length
-    int 0x80          ; call kernel
+    mov eax, sys_write
+    mov ebx, stdout
+    mov ecx, msg2  
+    mov edx, len2  
+    call_kernel
 
     ; sys_exit (status=0)
     mov eax, 1        ; syscall number for sys_exit
     xor ebx, ebx      ; exit code 0
-    int 0x80
+    call_kernel
